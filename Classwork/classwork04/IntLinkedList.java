a8a4795996529c62c180d847fe8db5687a37c7b6
jpatt <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-09-20 16:12:27 -0700

    initial blastoff

---
+++ Classwork/classwork04/IntLinkedList.java
@@ -0,0 +1,116 @@
+//File: IntLinkedList.Java
+//Author: Joshua Patterson
+//Date:9/20/18
+//Purpose: Implement removing and inserting index locations in lists
+//To compile: javac IntLinkedlist.java
+//To Run: java IntLinkedList
+
+public class IntLinkedList {
+
+     private Node head;
+     private int  size;
+
+    // the constructor
+     IntLinkedList() {
+        head = null;
+        size = 0;
+     }
+     //**************************************
+     public int getSize() {
+        return size;
+     }
+     //**************************************
+     public void prepend( int dataToAdd ) {
+        Node currentHead = head;
+        head = new Node( dataToAdd );
+        head.next = currentHead;
+        size++;
+     }
+     //**************************************
+     private class Node {
+
+        int data;            // remember this is an IntLinkedList
+        Node next;           // here's the self-referential part
+
+        // constructor
+        Node( int d ) {
+           data = d;
+           next = null;
+        }
+     }
+     //************************************
+     public Iterator getIteratorAt( int index ) {
+        if( (index > size) || (index < 0) ) {
+           throw new IllegalArgumentException();
+        }
+        Iterator it = new Iterator();
+        while( index > 0 ) {
+           it.next();
+           index--;
+        }
+        return it;
+     }
+     //************************************
+     public class Iterator {
+        private Node currentNode;
+
+        Iterator() {
+           currentNode = head;
+        }
+
+        public void next() {
+           if( currentNode == null ) {
+              return;
+           } else {
+              currentNode = currentNode.next;
+           }
+        }
+
+        public boolean hasNext() {
+           return ((currentNode != null) && (currentNode.next != null));
+        }
+
+        public int getCurrentInt() {
+           return currentNode.data;
+        }
+
+     }
+    //*************************************** Added Method insertAt()
+
+     public void insertAt(int index, int dataToAdd){
+       if(index != 0){
+          throw new IllegalArgumentException();
+       }
+       if(index == 0){
+          prepend(dataToAdd);
+       }
+       else {
+          Node Insert = new Node(dataToAdd);
+          Iterator InsertNum = getIteratorAt(index-1);
+
+          Insert.next = InsertNum.currentNode.next;
+          InsertNum.currentNode.next = Insert;
+          size++;
+       }
+    }
+
+    //*************************************** Added method removeAt()
+
+    public void removeAt(int index){
+        if( index >= size || index < 0)  {
+           throw new IllegalArgumentException();
+        }
+        if(index == 0){
+          this.head = head.next;
+          size--;
+        return;
+        }
+        Iterator Remove = new Iterator();
+
+        for(int i = 0; i < index - 1; i++){
+          it.next();
+        }
+        Remove.currentNode.next = Remove.currentNode.next.next; ///.. +1?
+        size--;
+    }
+}
