b4979e24eaaf53a0026e2f9a212b5d8d9e802ede
jpatt <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-10-20 14:41:28 -0700

    Create DequeApp.java

---
+++ Homework/Homework02/DequeApp.java
@@ -0,0 +1,32 @@
+class DequeApp{
+  public static void main(String[] args){
+
+    Deque theDeque = new Deque(5);
+
+    theDeque.insertLeft(10);
+    theDeque.insertLeft(20);
+    theDeque.insertLeft(30);
+    //theDeque.insertRight(40);
+
+    theDeque.insertLeft(50);
+    //theDeque.insertLeft(90);
+    /*theDeque.removeLeft();
+    theDeque.removeRight();
+    theDeque.removeRight();
+
+    theDeque.insertRight(50);
+    theDeque.insertLeft(60);
+    theDeque.insertLeft(70);
+    theDeque.insertLeft(80);
+*/
+    while( !theDeque.isEmpty() ){
+      long n = theDeque.removeRight(); // (40, 50, 60, 70, 80)
+      System.out.print(n);
+      System.out.print(" ");
+    }
+
+    System.out.println(" ");
+
+    theDeque.contentDequeDisplayer();
+    }
+  }
