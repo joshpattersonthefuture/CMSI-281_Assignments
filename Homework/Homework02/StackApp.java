c41b33926bc847ce0ab6964d7d3c84bb6815fe1a
jpatt <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-10-22 14:32:23 -0700

    Update StackApp.java

--- Homework/Homework02/StackApp.java
+++ Homework/Homework02/StackApp.java
@@ -1,5 +1,6 @@
-//Authors: Joshua Patterson & Rediet Teferi
+//Authors: Joshua Patterson & Rediet Teferi & Adam Reinart
 //Date: October 13, 2018
+//Purpose: Tester for Stack.java
 
 public class StackApp {
 
