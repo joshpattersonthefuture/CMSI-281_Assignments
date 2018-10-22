2209b65775372a9ac6d7f374dbb8c7a4a33741aa
jpatt <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-10-22 14:26:08 -0700

    Update DequeApp.java

--- Homework/Homework02/DequeApp.java
+++ Homework/Homework02/DequeApp.java
@@ -1,34 +1,54 @@
-//Authors: Joshua Patterson & Rediet Teferi
-//Date: October 15, 2018
-class DequeApp{
-  public static void main(String[] args){
+//Authors: Joshua Patterson & Rediet Teferi & Adam Reinart
+//Date: October 13, 2018
+//Purpose: The purpose of this is to implement a Deque
 
-    Deque theDeque = new Deque(5);
+public class DequeApp {
 
-    theDeque.insertLeft(10);
-    theDeque.insertLeft(20);
-    theDeque.insertLeft(30);
-    //theDeque.insertRight(40);
+    public static void main( String args[] ) {
+        Deque newDequeue = new Deque( 5 );
 
-    theDeque.insertLeft(50);
-    //theDeque.insertLeft(90);
-    /*theDeque.removeLeft();
-    theDeque.removeRight();
-    theDeque.removeRight();
+        System.out.println( "Test Insert Right");
+        newDequeue.insertRight( 4 );
+        newDequeue.insertRight( 20 );
+        newDequeue.insertRight( 15 );
+        newDequeue.insertRight( 32 );
+        newDequeue.displayDequeue();
 
-    theDeque.insertRight(50);
-    theDeque.insertLeft(60);
-    theDeque.insertLeft(70);
-    theDeque.insertLeft(80);
-*/
-    while( !theDeque.isEmpty() ){
-      long n = theDeque.removeRight(); // (40, 50, 60, 70, 80)
-      System.out.print(n);
-      System.out.print(" ");
+        System.out.println( "Test Remove Left");
+        newDequeue.removeLeft();
+        newDequeue.displayDequeue();
+        newDequeue.removeLeft();
+        newDequeue.displayDequeue();
+        newDequeue.removeLeft();
+        newDequeue.displayDequeue();
+
+        System.out.println( "Test Insert Left");
+        newDequeue.insertLeft( 18 );
+        newDequeue.insertLeft( 13 );
+        newDequeue.insertLeft( 24 );
+        newDequeue.insertLeft( 23 );
+        newDequeue.displayDequeue();
+
+        System.out.println( "Test Remove Right");
+        newDequeue.removeRight();
+        newDequeue.displayDequeue();
+        newDequeue.removeRight();
+        newDequeue.displayDequeue();
+
+        System.out.println( "Test Is Empty" );
+        System.out.println( " Dequeue empty? " + newDequeue.isEmpty() );  //false
+        System.out.println( " Dequeue full? " + newDequeue.isFull() );   //false
+        newDequeue.insertLeft( 143 );
+        newDequeue.insertRight( 26 );
+        newDequeue.displayDequeue();
+        //newDequeue.getSize();
+        System.out.println( " Dequeue full? " + newDequeue.isFull() );   //true
+        newDequeue.removeRight();
+        newDequeue.removeRight();
+        newDequeue.removeRight();
+        newDequeue.removeRight();
+        newDequeue.removeRight();
+        newDequeue.displayDequeue();
+        System.out.println( " Dequeue empty? " + newDequeue.isEmpty() );  //true
     }
-
-    System.out.println(" ");
-
-    theDeque.contentDequeDisplayer();
-    }
-  }
+}
